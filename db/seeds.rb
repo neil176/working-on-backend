4062ae0b0231b1fd813eeaa2f3a9361381819a6a
Neil Steiner <npsteiner@gmail.com>
2017-10-17 11:17:17 -0400

    added to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,66 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Seeding the database...."
 
 users = User.create([
 	{username: "neil176", password: "password", email: "n@n.com"},
-	{username: "other", password: "password", email: "o@nocom"}
+	{username: "other_guy", password: "password", email: "o@n.com"},
+	{username: "third_person", password: "password", email: "3@3.com"},
+	{username: "fourth_person", password: "password", email: "4@4.com"},
+
 ])
 
 follows = Follow.create([
-	{follower: User.all[0], followee: User.all[1]}
+	{follower: User.all[0], followee: User.all[1]},
+	{follower: User.all[0], followee: User.all[2]},
+	{follower: User.all[0], followee: User.all[3]},
+	{follower: User.all[1], followee: User.all[0]},
+	{follower: User.all[3], followee: User.all[1]},
+	{follower: User.all[3], followee: User.all[2]}
+
 ])
 
 projects = Project.create([
-	{title: "my first project", user_id: 1}
+	{title: "my first project", user_id: 1},
+	{title: "my second project", user_id: 1},
+	{title: "my third project", user_id: 1},
+
+	{title: "user2's first project", user_id: 2},
+	{title: "user2's second project", user_id: 2},
+	{title: "user2's third project", user_id: 2},
+
+
+	{title: "user3's first project", user_id: 3},
+	{title: "user3's second project", user_id: 3},
+	{title: "user3's third project", user_id: 3},
+
 ])
 
 items = Item.create([
-	{description: "item number one", project_id: 1}
+	{description: "item number one", project_id: 1, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 2", project_id: 1, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 3", project_id: 1, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 4", project_id: 2, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 5", project_id: 2, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 6", project_id: 2, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 7", project_id: 3, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 8", project_id: 3, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 9", project_id: 3, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 10", project_id: 4, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 11", project_id: 4, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 12", project_id: 5, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 13", project_id: 5, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 14", project_id: 6, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 15", project_id: 6, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 16", project_id: 7, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 17", project_id: 7, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 18", project_id: 8, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 19", project_id: 9, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+	{description: "item number 20", project_id: 1, image: "https://i.imgur.com/UL8gyZi.jpg?1"},
+
 ])
+
+
+puts "Seeding complete!"
 	
