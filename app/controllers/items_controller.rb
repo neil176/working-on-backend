9d85e91c6f87987aef035bf8fa99b25df9f5796b
Neil Steiner <npsteiner@gmail.com>
2017-10-19 18:05:00 -0400

    added items controller for item creation fetches

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+class ItemsController < ApplicationController
+
+	def create
+		# binding.pry
+		@item = Item.create(item_params[:item])
+		render json: @item.project
+	end
+
+
+	private
+
+	def item_params
+		params.permit(item:[:description, :image, :project_id])
+	end
+
+end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,15 @@ class ProjectsController < ApplicationController
 		@project = Project.find(params[:id])
 		render json: @project
 	end
+
+	def create
+		@project = Project.create(project_params)
+		render json: @project
+	end
+
+	private
+
+	def project_params
+		params.permit(:user_id, :title)
+	end
 end
